5d296590df04fca986410ad1c57c189182f24303
4L3SS104 <russoalessio2004@gmail.com>
2026-06-09 17:07:51 +0200

    MOD: Sistemazione ombre progressbar

--- src/components/inputs/ClayProgressBar.vue
+++ src/components/inputs/ClayProgressBar.vue
@@ -9,8 +9,10 @@
 </script>
 
 <template>
-    <label class="clay-progress-bar">
-        <span class="clay-progress-bar__indicator" :style="{ width: `${progress}%` }"></span>
+    <label class="clay-progress-bar" :style="{ '--_w': `${progress}%` }">
+        <span class="clay-progress-bar__track">
+            <span class="clay-progress-bar__indicator" :style="{ width: `${progress}%` }"></span>
+        </span>
     </label>
 </template>
 
@@ -23,47 +25,73 @@
         --clay-progress-bar-background-color: oklch(from var(--clay-light-color) calc(l - 0.15) c h);
         --clay-progress-bar-indicator-color: var(--clay-primary-color);
         --clay-progress-bar-indicator-color-shadow-background:
-            inset 0 0.25em 0.25em 0 rgba(from var(--black) r g b / 0.025);
+            inset 0 0.25em 0.25em 0 rgba(from var(--black) r g b / 0.125);
         --clay-progress-bar-shadow-color: var(--black);
 
         --clay-border-radius: 0.35rem;
         --clay-progress-bar-height: 0.75rem;
     }
 
-    .clay-progress-bar
+    .clay-progress-bar // contenitore
     {
         display: block;
         position: relative;
         width: 100%;
         height: var(--clay-progress-bar-height);
-        background-color: var(--clay-progress-bar-background-color);
         border-radius: var(--clay-border-radius);
-        overflow: hidden;
         z-index: 0;
-        @include mixins.clay-shadow-elevation($color: var(--clay-progress-bar-shadow-color), $intensity: 0.5);
+
+        &::after // ombra della progress bar
+        {
+            border-radius: var(--clay-border-radius);
+            bottom: 0;
+            content: "";
+            left: 0;
+            pointer-events: none;
+            position: absolute;
+            top: 0;
+            transition: width 0.2s ease;
+            width: var(--_w, 0%);
+            z-index: 1;
+            @include mixins.clay-shadow-elevation($intensity: 0.5);
+        }
+
+        &__track // background dove la progress bar si muove
+        {
+            background-color: var(--clay-progress-bar-background-color);
+            border-radius: var(--clay-border-radius);
+            bottom: 0;
+            box-shadow: var(--clay-progress-bar-indicator-color-shadow-background);
+            left: 0;
+            overflow: hidden;
+            position: absolute;
+            right: 0;
+            top: 0;
+        }
 
         &__indicator
         {
             display: block;
+            align-items: center;
+            position: absolute;
             height: 100%;
             background-color: var(--clay-progress-bar-indicator-color);
             transition: width 0.2s ease;
             border-radius: var(--clay-border-radius);
             z-index: 0;
-            @include mixins.clay-shadow-puff($intensity: 0.1);
 
-            &::before
-            {
-                content: "";
-                position: absolute;
-                top: 0;
-                left: 0;
-                right: 0;
-                bottom: 0;
-                border-radius: var(--clay-border-radius);
-                box-shadow: var(--clay-progress-bar-indicator-color-shadow-background);
-                z-index: -1;
-            }
+        &::before
+                {
+                    content: "";
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    right: 0;
+                    bottom: 0;
+                    border-radius: var(--clay-border-radius);
+                    @include mixins.clay-shadow-puff($intensity: 0.25);
+                    z-index: -1;
+                }
         }
     }
 
